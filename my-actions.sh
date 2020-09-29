5e461756dfe1e99ece0070f8ee30096014fa6689
Christine Harris <60186199+ChristineHarris@users.noreply.github.com>
2020-09-28 21:14:32 -0400

    Add files via upload

---
+++ my-actions.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python -m pip install seaborn==0.10.0
+python -m pip install matplotlib
+python -m pip install numpy
+python -m pip install scipy
+python -m pip install --upgrade scikit-image
